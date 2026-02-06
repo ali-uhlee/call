596da29b4af567e070c8fc6fc2e0f01e736c79e4
Ali <breadsticks68@gmail.com>
2026-02-06 13:13:30 -0500

    Modify service worker caching strategy
    Updated service worker to cache new files and paths.

--- sw.js
+++ sw.js
@@ -1,19 +1,12 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("countdown-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("countdown-v1").then(cache =>
+      cache.addAll([
+        "/call/",
+        "/call/index.html",
+        "/call/icon.png",
+        "/call/manifest.json"
+      ])
+    )
   );
 });
